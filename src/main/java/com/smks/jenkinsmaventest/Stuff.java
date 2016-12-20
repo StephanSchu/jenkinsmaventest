8d2e79835c3b566268c678ebb233765d73a6e80d
stephanschu <schu.stephan@gmail.com>
2016-12-20 11:22:52 +0100

    test commit

---
+++ src/main/java/com/smks/jenkinsmaventest/Stuff.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smks.jenkinsmaventest;
+
+/**
+ *
+ * @author stephanschu
+ */
+public class Stuff {
+    public void doStuff(){
+        System.out.println("I do stuff");
+    }
+}
