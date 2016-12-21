240bf8f9b74721c3570a042e3dad2abac24c40f7
StephanSchu <schu.stephan@gmail.com>
2016-12-21 11:57:03 +0100

    added more stuff

---
+++ src/main/java/com/smks/jenkinsmaventest/MoreStuff.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smks.jenkinsmaventest;
+
+/**
+ *
+ * @author stephanschu
+ */
+public class MoreStuff {
+    
+    public void stuff(){
+        System.out.println("I do more stuff");
+    }
+}
